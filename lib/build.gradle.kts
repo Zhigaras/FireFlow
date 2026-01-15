ff751ed76d6a8d029e84bb115ca00379d5f4e5ee
Ilya Zhigaras <i.zhigaras@vkteam.ru>
2026-01-16 00:53:20 +0300

    adjust

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -37,11 +37,4 @@ android {
 dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.database)
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
